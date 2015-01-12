31306ed6313ebf36c49200cee685d6bd88e3430a
Harlow Ward <harlow@hoteltonight.com>
2015-01-12 11:44:54 -0800

    Delete goose file for Heroku deployment

--- auth_service/install_goose.go
+++
@@ -1,7 +0,0 @@
-// use build constraints to work around http://code.google.com/p/go/issues/detail?id=4210
-// +build heroku
-
-// note: need at least one blank line after build constraint
-package main
-
-import _ "bitbucket.org/liamstask/goose/cmd/goose"
