fc611d49f9daabefeb370b6af647dc585f732734
Harlow Ward <harlow@hward.com>
2015-09-10 18:44:42 -0700

    Remove global flags from Rate Service

--- service.rate/main.go
+++ service.rate/main.go
@@ -17,11 +17,12 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-var (
-	port       = flag.Int("port", 10004, "The server port")
-	jsonDBFile = flag.String("json_db_file", "data/rates.json", "A json file containing a list of rate plans")
-	serverName = "service.rate"
-)
+// newServer returns a server with initialization data loaded.
+func newServer(dataPath string) *rateServer {
+	s := &rateServer{serverName: "service.rate"}
+	s.loadRates(dataPath)
+	return s
+}
 
 type stay struct {
 	HotelID int32
@@ -30,15 +31,17 @@ type stay struct {
 }
 
 type rateServer struct {
-	rates map[stay]*pb.RatePlan
+	serverName string
+	rates      map[stay]*pb.RatePlan
 }
 
 // GetRates gets rates for hotels for specific date range.
 func (s *rateServer) GetRates(ctx context.Context, args *pb.Args) (*pb.Reply, error) {
 	md, _ := metadata.FromContext(ctx)
+
 	t := trace.Tracer{TraceID: md["traceID"]}
-	t.In(serverName, md["from"])
-	defer t.Out(md["from"], serverName, time.Now())
+	t.In(s.serverName, md["from"])
+	defer t.Out(md["from"], s.serverName, time.Now())
 
 	reply := new(pb.Reply)
 	for _, hotelID := range args.HotelIds {
@@ -46,6 +49,7 @@ func (s *rateServer) GetRates(ctx context.Context, args *pb.Args) (*pb.Reply, er
 		if s.rates[k] == nil {
 			continue
 		}
+
 		reply.RatePlans = append(reply.RatePlans, s.rates[k])
 	}
 
@@ -58,10 +62,12 @@ func (s *rateServer) loadRates(filePath string) {
 	if err != nil {
 		log.Fatalf("Failed to load file: %v", err)
 	}
+
 	rates := []*pb.RatePlan{}
 	if err := json.Unmarshal(file, &rates); err != nil {
 		log.Fatalf("Failed to load json: %v", err)
 	}
+
 	s.rates = make(map[stay]*pb.RatePlan)
 	for _, ratePlan := range rates {
 		k := stay{ratePlan.HotelId, ratePlan.InDate, ratePlan.OutDate}
@@ -69,20 +75,19 @@ func (s *rateServer) loadRates(filePath string) {
 	}
 }
 
-// newServer returns a server with initialization data loaded.
-func newServer() *rateServer {
-	s := new(rateServer)
-	s.loadRates(*jsonDBFile)
-	return s
-}
-
 func main() {
+	var (
+		port     = flag.Int("port", 10004, "The server port")
+		dataPath = flag.String("data_path", "data/rates.json", "A json file containing a list of rate plans")
+	)
 	flag.Parse()
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	grpcServer := grpc.NewServer()
-	pb.RegisterRateServer(grpcServer, newServer())
+	pb.RegisterRateServer(grpcServer, newServer(*dataPath))
 	grpcServer.Serve(lis)
 }
