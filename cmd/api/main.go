dbc55574a039c6acb508a7ad47c45402a7a79fa4
Harlow Ward <harlow@hward.com>
2016-08-28 11:36:04 -0700

    Pass gRPC connections to HTTP handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	_ "expvar"
 	"flag"
@@ -13,55 +14,43 @@ import (
 	"github.com/harlow/go-micro-services/pb/profile"
 	"github.com/harlow/go-micro-services/pb/rate"
 
+	uuid "github.com/nu7hatch/gouuid"
+
 	"github.com/harlow/authtoken"
-	"golang.org/x/net/context"
 	"golang.org/x/net/trace"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
-
-// "github.com/nu7hatch/gouuid"
-// 	traceID, _ := uuid.NewV4()
-// 	traceID.String()
 
 type inventory struct {
 	Hotels    []*profile.Hotel `json:"hotels"`
 	RatePlans []*rate.RatePlan `json:"ratePlans"`
 }
 
-type service struct {
-	serverName string
-
+type client struct {
 	auth.AuthClient
 	geo.GeoClient
 	profile.ProfileClient
 	rate.RateClient
 }
 
-// mustDial ensures the tcp connection to specified address.
-func mustDial(addr *string) *grpc.ClientConn {
-	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("failed to dial: %v", err)
-		panic(err)
-	}
-	return conn
-}
-
-func (s service) requestHandler(w http.ResponseWriter, r *http.Request) {
+func requestHandler(c client, w http.ResponseWriter, r *http.Request) {
 	// tracing
-	tr := trace.New(s.serverName, "URL PATH!")
+	tr := trace.New("api.v1", r.URL.Path)
 	defer tr.Finish()
-
-	// metadata
-	md := metadata.Pairs("traceID", "TRACEID", "fromName", s.serverName)
 
 	// context
 	ctx := context.Background()
 	ctx = trace.NewContext(ctx, tr)
-	ctx = metadata.NewContext(ctx, md)
 
-	// grab auth token from request
+	// add metadata to context for grpc calls
+	traceID, _ := uuid.NewV4()
+	ctx = metadata.NewContext(ctx, metadata.Pairs(
+		"traceID", traceID.String(),
+		"fromName", "api.v1",
+	))
+
+	// token from request headers
 	token, err := authtoken.FromRequest(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusForbidden)
@@ -69,13 +58,15 @@ func (s service) requestHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// verify token w/ auth service
-	_, err = s.VerifyToken(ctx, &auth.Request{token})
+	_, err = c.VerifyToken(ctx, &auth.Request{
+		AuthToken: token,
+	})
 	if err != nil {
-		http.Error(w, "Unauthorized", http.StatusForbidden)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
 
-	// read and validate in/out arguments
+	// query params
 	inDate := r.URL.Query().Get("inDate")
 	outDate := r.URL.Query().Get("outDate")
 	if inDate == "" || outDate == "" {
@@ -84,19 +75,19 @@ func (s service) requestHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// finds nearby hotels
-	geoRes, err := s.Nearby(ctx, &geo.Request{
+	// TODO(hw): use lat/lon from request params
+	geoRes, err := c.Nearby(ctx, &geo.Request{
 		Lat: 51.502973,
 		Lon: -0.114723,
 	})
-
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	// make reqeusts for profiles and rates
-	profileCh := s.getProfiles(ctx, geoRes.HotelIds)
-	rateCh := s.getRatePlans(ctx, geoRes.HotelIds, inDate, outDate)
+	profileCh := getHotelProfiles(c, ctx, geoRes.HotelIds)
+	rateCh := getRatePlans(c, ctx, geoRes.HotelIds, inDate, outDate)
 
 	// wait on profiles reply
 	profileReply := <-profileCh
@@ -112,30 +103,12 @@ func (s service) requestHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// build the final inventory response
-	inventory := inventory{
+	// render inventory response
+	p := inventory{
 		Hotels:    profileReply.hotels,
 		RatePlans: rateReply.ratePlans,
 	}
-
-	// encode JSON for rendering
-	encoder := json.NewEncoder(w)
-	if err = encoder.Encode(inventory); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func (s service) getRatePlans(ctx context.Context, hotelIDs []string, inDate string, outDate string) chan rateResults {
-	ch := make(chan rateResults, 1)
-
-	go func() {
-		req := &rate.Request{hotelIDs, inDate, outDate}
-		res, err := s.GetRates(ctx, req)
-		ch <- rateResults{res.RatePlans, err}
-	}()
-
-	return ch
+	json.NewEncoder(w).Encode(p)
 }
 
 type rateResults struct {
@@ -143,13 +116,16 @@ type rateResults struct {
 	err       error
 }
 
-func (s service) getProfiles(ctx context.Context, hotelIDs []string) chan profileResults {
-	ch := make(chan profileResults, 1)
+func getRatePlans(c client, ctx context.Context, hotelIDs []string, inDate string, outDate string) chan rateResults {
+	ch := make(chan rateResults, 1)
 
 	go func() {
-		req := &profile.Request{hotelIDs, "en"}
-		res, err := s.GetProfiles(ctx, req)
-		ch <- profileResults{res.Hotels, err}
+		res, err := c.GetRates(ctx, &rate.Request{
+			HotelIds: hotelIDs,
+			InDate:   inDate,
+			OutDate:  outDate,
+		})
+		ch <- rateResults{res.RatePlans, err}
 	}()
 
 	return ch
@@ -160,6 +136,32 @@ type profileResults struct {
 	err    error
 }
 
+func getHotelProfiles(c client, ctx context.Context, hotelIDs []string) chan profileResults {
+	ch := make(chan profileResults, 1)
+
+	go func() {
+		res, err := c.GetProfiles(ctx, &profile.Request{
+			HotelIds: hotelIDs,
+			Locale:   "en",
+		})
+		ch <- profileResults{res.Hotels, err}
+	}()
+
+	return ch
+}
+
+// mustDial ensures a tcp connection to specified address.
+func mustDial(addr *string) *grpc.ClientConn {
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("failed to dial: %v", err)
+		panic(err)
+	}
+
+	return conn
+}
+
 func main() {
 	// trace library patched for demo purposes.
 	// https://github.com/golang/net/blob/master/trace/trace.go#L94
@@ -167,6 +169,7 @@ func main() {
 		return true, true
 	}
 
+	// ports for grpc connections (default uses docker-compose links)
 	var (
 		port        = flag.String("port", "8080", "The server port")
 		authAddr    = flag.String("auth", "auth:8080", "The Auth server address in the format of host:port")
@@ -176,13 +179,17 @@ func main() {
 	)
 	flag.Parse()
 
-	svc := service{
-		serverName:    "api.v1",
+	// client with all grpc connections
+	c := client{
 		AuthClient:    auth.NewAuthClient(mustDial(authAddr)),
 		GeoClient:     geo.NewGeoClient(mustDial(geoAddr)),
 		ProfileClient: profile.NewProfileClient(mustDial(profileAddr)),
 		RateClient:    rate.NewRateClient(mustDial(rateAddr)),
 	}
-	http.HandleFunc("/", svc.requestHandler)
+
+	// handle http requests
+	http.HandleFunc("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestHandler(c, w, r)
+	}))
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
